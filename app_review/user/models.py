ea80ea02f7a54e8fe3cedb3f04a7527278fdb6a2
Colin Morgan <colin.morgan@fullbit.ca>
2017-07-24 13:23:24 -0230

    Add password hasing handling and github state token to User

--- app_review/user/models.py
+++ app_review/user/models.py
@@ -1,3 +1,6 @@
+import uuid
+from werkzeug.security import (generate_password_hash,
+                               check_password_hash)
 from app_review.extensions import db
 
 
@@ -6,4 +9,25 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True)
-    github_access_token = db.Column(db.String, unique=True)
+    github_access_token = db.Column(db.String, unique=True,
+                                    nullable=True)
+    github_state_token = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.set_password(password)
+        self.set_github_state()
+
+    def check_password(self, password):
+        """Compare a string versus a hashed password"""
+        return check_password_hash(self.password, password)
+
+    def set_password(self, password):
+        """Create a salted and hashed password given a string"""
+        self.password = generate_password_hash(password)
+
+    def set_github_state(self):
+        """Create a random token to match a
+           callback response with a user"""
+        self.github_state_token = uuid.uuid4().hex
