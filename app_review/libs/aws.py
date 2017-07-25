9c29564a9d123f7c6c49701b53e5fc6eea6cbe4d
Colin Morgan <colin.morgan@fullbit.ca>
2017-07-25 18:00:34 -0230

    Refactor, add waits, add existing instance_id handling

--- app_review/libs/aws.py
+++ app_review/libs/aws.py
@@ -1,3 +1,4 @@
+import time
 import boto.ec2
 from flask import current_app
 
@@ -22,7 +23,8 @@ class AWS(object):
 class EC2(AWS):
     """Creates an ec2 image object"""
 
-    def __init__(self, ami_id, instance_type):
+    def __init__(self, instance_id=None, ami_id='ami-5c557539',
+                 instance_type='t2.micro'):
         super(EC2, self).__init__()
 
         self.connection = boto.ec2.connect_to_region(
@@ -32,26 +34,26 @@ class EC2(AWS):
         )
         self.ami_id = ami_id
         self.instance_type = instance_type
-
-    @property
-    def _instance(self):
-        """Returns an instance object, if a reservation exists"""
-        if self.reservation:
-            return self.reservation.instances[0]
+        self._reservation = None
+        if instance_id:
+            reservations = self.connection.get_all_reservations(
+                instance_id)
+            if reservations:
+                self._reservation = reservations[0]
 
     @property
     def _permitted_groups(self):
         """Returns a list of permitted groups,
            if a reservation exists.
         """
-        if self.reservation:
-            return self.reservation.groups
+        if self._reservation:
+            return self._reservation.groups
 
     @property
     def _owner_id(self):
         """Returns an owner id, if a reservation exists"""
-        if self.reservation:
-            return self.reservation.owner_id
+        if self._reservation:
+            return self._reservation.owner_id
 
     @property
     def _security_groups(self):
@@ -65,31 +67,49 @@ class EC2(AWS):
         """Returns AWS region to use"""
         return 'us-east-2'
 
-    def _run_instance(self):
-        """Starts the instance"""
-        self.reservation = self.connection.run_instances(
+    def _wait_for_status(self, status):
+        while self.instance.state != status:
+            time.sleep(10)
+            self.instance.update()
+
+    def _create_and_start_instance(self):
+        """Create and starts an instance"""
+        self._reservation = self.connection.run_instances(
             self.ami_id,
             instance_type=self.instance_type,
             security_groups=self._security_groups,
             key_name=self._key_name)
+        self._wait_for_status('running')
+
+    def _start_instance(self):
+        """Starts the instance"""
+        if (self.instance and self.state != 'running' and
+                              self.state != 'terminated'):
+            self.instance.start()
+            self._wait_for_status('running')
 
     def _stop_instance(self):
         """Stops the instance"""
-        if self._instance:
-            self.connection.stop_instances(self._instance.id)
+        if self.instance:
+            self.connection.stop_instances(self.instance.id)
+            self._wait_for_status('stopped')
 
     def _terminate_instance(self):
         """Terminates the instance"""
-        if self._instance:
-            self.connection.terminate_instances(self._instance.id)
+        if self.instance:
+            self.connection.terminate_instances(self.instance.id)
+            self._wait_for_status('terminated')
 
-    def _get_instance_status(self):
+    def _get_instance_state(self):
         """Returns the instances status"""
-        if self._instance:
-            return self._instance.update()
+        if self.instance:
+            return self.instance.update()
 
     def start(self):
-        self._run_instance()
+        if self.instance:
+            self._start_instance()
+        else:
+            self._create_and_start_instance()
 
     def stop(self):
         self._stop_instance()
@@ -97,5 +117,12 @@ class EC2(AWS):
     def terminate(self):
         self._terminate_instance()
 
-    def status(self):
-        return self._get_instance_status()
+    @property
+    def state(self):
+        return self._get_instance_state()
+
+    @property
+    def instance(self):
+        if self._reservation:
+            return self._reservation.instances[0]
+
