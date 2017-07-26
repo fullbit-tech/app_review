3d4d1882fede6ff2e5cbf802e1cf6d7f0aaa7a70
Colin Morgan <colin.morgan@fullbit.ca>
2017-07-26 16:48:54 -0230

    Add basic recipe handling during instance creation

--- app_review/instance/views.py
+++ app_review/instance/views.py
@@ -7,6 +7,7 @@ from app_review.libs.aws import EC2
 from app_review.extensions import db
 from app_review.instance.schemas import pull_request_schema
 from app_review.instance.models import PullRequestInstance
+from app_review.recipe.models import Recipe
 
 
 instance_api_bp = Blueprint('instance_api', __name__)
@@ -27,7 +28,7 @@ class PullRequest(Resource):
             pull_request = gh.get_pull_request(
                 owner, repo, number)
         except GitHubException:
-            abort(404, message="Not Found")
+            abort(404, message="Pull Request Not Found")
         return pull_request
 
     def _get_instance(self, owner, repo, number, user):
@@ -36,6 +37,13 @@ class PullRequest(Resource):
             github_pull_number=number, user_id=user.id,
         ).filter(
             PullRequestInstance.instance_state != 'terminated').first()
+
+    def _get_recipe(self, recipe_id, user):
+        recipe = Recipe.query.filter_by(
+            id=recipe_id, user_id=user.id).first()
+        if not recipe:
+            abort(500, message="Recipe Not Found")
+        return recipe
 
     @jwt_required()
     def get(self, owner, repo, number):
@@ -47,17 +55,24 @@ class PullRequest(Resource):
     @jwt_required()
     def post(self, owner, repo, number):
         """Starts an instance for a pull request"""
+        payload = request.get_json()
         self._get_pull_request(owner, repo, number)
+        recipe = None
+        recipe_id = payload['recipe']
+
         instance = self._get_instance(owner, repo, number, g.user)
+        if recipe_id:
+            recipe = self._get_recipe(recipe_id, g.user)
+
         ec2 = EC2(instance.instance_id if instance else None)
-        ec2.start()
+        ec2.start(recipe.script if recipe else None)
         if not instance:
             instance = PullRequestInstance(
-                ec2.instance.id, ec2.state,
+                ec2.instance.id,
+                ec2.state,
                 ec2.instance.instance_type,
                 ec2.instance.public_dns_name,
-                owner, repo, number, g.user,
-            )
+                owner, repo, number, g.user, recipe)
         else:
             instance.state = ec2.state
         db.session.add(instance)
